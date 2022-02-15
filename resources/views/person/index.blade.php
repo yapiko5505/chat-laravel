273121aab4ee2d3de4a82ee42d7d715ae2216fd2
yapiko <bloomingjewels7021@gmail.com>
2022-02-15 14:45:35 +0900

    chat-laravel14

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -6,11 +6,11 @@
     @parent
     インデックスページ
     <div class="col-sm-2">
-    @if (Auth::check())
-        <p>USER: {{$user->name . '(' . $user->email . ')'}}<p>
-    @else
-        <p>ログインしていません。<br>(<a href="/login">ログイン</a> | <a href="/register">登録</a></p>) 
-    @endif
+    <!-- @if (Auth::check()) -->
+        <!-- <p>USER: {{$user->name . '(' . $user->email . ')'}}<p> -->
+    <!-- @else -->
+        <!-- <p>ログインしていません。<br>(<a href="/login">ログイン</a> | <a href="/register">登録</a></p>)  -->
+    <!-- @endif -->
         <br><a href="/person/find" class="btn btn-warning"><i class="glyphicon glyphicon-plus"></i>検索ページ</a><br>
         <a href="/board" class="btn btn-warning"><i class="glyphicon glyphicon-plus"></i>ボードページ</a><br>
         <a href="{{ url('/home') }}">ログアウト</a> 
